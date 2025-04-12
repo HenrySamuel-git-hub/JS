96e7fa9ad458e63862d2a6aa9754b8ad2070c0da
HenrySamuel-git-hub <henrysamuel743@gmail.com>
2025-04-12 23:35:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function trafficsignal(Tcolors){
+    document.getElementById('red').style.backgroundColor = 'gray';
+    document.getElementById('yellow').style.backgroundColor = 'gray';
+    document.getElementById('green').style.backgroundColor = 'gray';
+
+    document.getElementById(Tcolors).style.backgroundColor = Tcolors;
+
+    
+
+}
